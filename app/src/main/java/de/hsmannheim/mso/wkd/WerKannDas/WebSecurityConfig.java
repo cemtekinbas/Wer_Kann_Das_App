066654033659152f2a32dbba426d939db0f88ade
Christian <oxi@keks.li>
2017-11-23 11:13:41 +0100

    copied html files to project

--- app/src/main/java/de/hsmannheim/mso/wkd/WerKannDas/WebSecurityConfig.java
+++ app/src/main/java/de/hsmannheim/mso/wkd/WerKannDas/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/home", "/css", "/font", "/fonts", "/img", "/js", "/sass").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
