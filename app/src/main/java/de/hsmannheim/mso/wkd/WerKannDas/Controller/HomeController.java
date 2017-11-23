f041aa3bfe9102fddf60c48d6ba3ced99c3abc50
Christian <oxi@keks.li>
2017-11-23 10:09:06 +0100

    base spring project files

---
+++ app/src/main/java/de/hsmannheim/mso/wkd/WerKannDas/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package de.hsmannheim.mso.wkd.WerKannDas.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String dashboard() {
+		return "home";
+	}
+	
+}
---
+++ app/src/main/java/de/hsmannheim/mso/wkd/WerKannDas/WerKannDasApplication.java
@@ -0,0 +1,12 @@
+package de.hsmannheim.mso.wkd.WerKannDas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerKannDasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerKannDasApplication.class, args);
+	}
+}
