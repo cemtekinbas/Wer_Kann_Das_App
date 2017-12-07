a366593696d0b69cd8968210a2b4956b206a71b7
MissesV <Cristin.volz@googlemail.com>
2017-12-07 09:07:58 +0100

    html und css eingefügt

--- app/src/main/java/de/hsmannheim/mso/wkd/WerKannDas/WebSecurityConfig.java
+++ app/src/main/java/de/hsmannheim/mso/wkd/WerKannDas/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/home", "/css", "/font", "/fonts", "/img", "/js", "/sass").permitAll()
+                .antMatchers("/", "/home", "/css/**", "/font/**", "/fonts/**", "/img/**", "/js/**", "/sass/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
