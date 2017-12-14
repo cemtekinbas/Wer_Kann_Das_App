8a5945444d6fd84ada646f3f6e547cb63f1e1b5a
Christian <oxi@keks.li>
2017-12-14 09:53:55 +0100

    fixed cast error in home controller

--- app/src/main/java/de/hsmannheim/mso/wkd/WerKannDas/Controller/HomeController.java
+++ app/src/main/java/de/hsmannheim/mso/wkd/WerKannDas/Controller/HomeController.java
@@ -25,52 +25,61 @@ public class HomeController {
 
 	@RequestMapping(value={"","/"}, method=RequestMethod.GET)
 	public String dashboard(Model model) {
-	    //String username = ((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
-        String username = (String)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-	    User user = userService.getByName(username);
-	    List<Request> requests = requestService.getList();
-		model.addAttribute("requestList", requests);
+	    if(SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof org.springframework.security.core.userdetails.User) {
+            String username = ((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+            User user = userService.getByName(username);
+            List<Request> requests = requestService.getList();
+            model.addAttribute("requestList", requests);
+            return "hello";
+        }
 	    return "home";
 	}
 
     @RequestMapping(value={"","/"}, method=RequestMethod.GET, params = {"search"})
     public String dashboardSearch(@RequestParam("search") String search, Model model) {
-        String username = ((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
-        User user = userService.getByName(username);
-        List<Request> requests = requestService.getList(search);
-        model.addAttribute("requestList", requests);
+        if(SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof org.springframework.security.core.userdetails.User) {
+            String username = ((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+            User user = userService.getByName(username);
+            List<Request> requests = requestService.getList(search);
+            model.addAttribute("requestList", requests);
+            return "hello";
+        }
 	    return "home";
     }
 
     @RequestMapping(value={"","/"}, method=RequestMethod.GET, params = {"sort"})
     public String dashboardSort(@RequestParam("sort") String sort, Model model) {
-        String username = ((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
-        User user = userService.getByName(username);
-        List<Request> requests = requestService.getList();
-        switch(sort)
-        {
-            case "up":
-                break;
-            case "down":
-                break;
+        if(SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof org.springframework.security.core.userdetails.User) {
+            String username = ((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+            User user = userService.getByName(username);
+            List<Request> requests = requestService.getList();
+            switch (sort) {
+                case "up":
+                    break;
+                case "down":
+                    break;
+            }
+            model.addAttribute("requestList", requests);
+            return "hello";
         }
-        model.addAttribute("requestList", requests);
 	    return "home";
     }
 
     @RequestMapping(value={"","/"}, method=RequestMethod.GET, params = {"search", "sort"})
     public String dashboard(@RequestParam("search") String search, @RequestParam("sort") String sort, Model model) {
-        String username = ((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
-        User user = userService.getByName(username);
-        List<Request> requests = requestService.getList(search);
-        switch(sort)
-        {
-            case "up":
-                break;
-            case "down":
-                break;
+        if(SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof org.springframework.security.core.userdetails.User) {
+            String username = ((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+            User user = userService.getByName(username);
+            List<Request> requests = requestService.getList(search);
+            switch (sort) {
+                case "up":
+                    break;
+                case "down":
+                    break;
+            }
+            model.addAttribute("requestList", requests);
+            return "hello";
         }
-        model.addAttribute("requestList", requests);
         return "home";
     }
 
