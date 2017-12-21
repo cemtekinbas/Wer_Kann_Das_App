cc02c739c57690892f378c451ea2af3733f514c9
Christian <oxi@keks.li>
2017-12-21 11:04:01 +0100

    fixed stackoverflow when authenticatoin failed and added logout capability

--- app/src/main/java/de/hsmannheim/mso/wkd/WerKannDas/MvcConfig.java
+++ app/src/main/java/de/hsmannheim/mso/wkd/WerKannDas/MvcConfig.java
@@ -26,8 +26,8 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         //registry.addViewController("/home").setViewName("home");
         //registry.addViewController("/").setViewName("home");
-        //registry.addViewController("/login").setViewName("login");
-        //registry.addViewController("/logout").setViewName("logout");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/logout").setViewName("home");
     }
 
     @Bean(name = "bcryptPasswordEncoder")
